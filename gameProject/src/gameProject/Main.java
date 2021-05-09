8db106f899492a3855ad5f26c44098c6d3dd39a6
SONNUR TAYFUROĞLU <2014141005@cumhuriyet.edu.tr>
2021-05-09 22:42:25 +0300

    09May

--- gameProject/src/gameProject/Main.java
+++ gameProject/src/gameProject/Main.java
@@ -38,7 +38,6 @@ public class Main {
 			         new Campaign(1, "New Year Campaign",50)
 			         );
 		System.out.println("                                                  ");
-		System.out.println("                                                  ");
 	}
 
 }
